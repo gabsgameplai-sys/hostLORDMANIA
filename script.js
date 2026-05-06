30479756cd1a63aad6ae736fcd4399bf55ef36e8
lordmania <251746943+gabsgameplai-sys@users.noreply.github.com>
2026-05-06 12:30:42 -0300

    Add send function for chat interactions

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+function send() {
+  let input = document.getElementById("msg");
+  let chat = document.getElementById("chat");
 
+  let userMsg = input.value.trim();
+  if (!userMsg) return;
+
+  chat.innerHTML += `<p class="user"><b>Você:</b> ${userMsg}</p>`;
+
+  let reply = "";
+
+  if (userMsg === "/help") {
+    reply = "Comandos: /help, /status, /ip, /skin NICK";
+  }
+
+  else if (userMsg === "/status") {
+    reply = "Servidor: Online 🟢";
+  }
+
+  else if (userMsg === "/ip") {
+    reply = "IP: lordmania.qzz.io";
+  }
+
+  else if (userMsg.startsWith("/skin")) {
+    let nick = userMsg.split(" ")[1];
+
+    if (!nick) {
+      reply = "Use: /skin NICK";
+    } else {
+      reply = `
+        Skin de ${nick}:<br>
+        <img src="https://crafatar.com/renders/body/${nick}">
+      `;
+    }
+  }
+
+  else {
+    reply = "Comando desconhecido. Use /help";
+  }
+
+  chat.innerHTML += `<p class="bot"><b>Bot:</b> ${reply}</p>`;
+
+  chat.scrollTop = chat.scrollHeight;
+  input.value = "";
+}
